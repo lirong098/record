17d554e815dded72b00866ff4f6e48e71d601fcd
李荣 <lirong@peilian.com>
2018-05-29 22:04:18 +0800

    ok

--- teacherList.lua
+++ teacherList.lua
@@ -7,8 +7,8 @@ wrk.headers["userId"] = "123"
 wrk.body = '{ "timeZone": "+8", "studentId": 350692, "instrumentType": 1, "beginTimeStr": "2018-05-30", "beginTime": 1527609600, "classTime": [{ "classStartTime": "15:00", "classTimeType": 1, "dayOfWeek": 3, "g8t": "3-15:00", "time": 900 }] }'
 `wrk.path  = "/guarder/v1/fixedClass/teacherList"
 
--- https://guarder-v1-pre.peilian.com/guarder/v1/fixedClass/teacherList
+-- https://${'需要测试的域名'}/guarder/v1/fixedClass/teacherList
 
--- wrk -t 4 -c 500 -d 30s -s teacherList.lua http://guarder-v1-pre.peilian.com
+-- wrk -t 4 -c 500 -d 30s -s teacherList.lua http://${需要测试的域名}
 
 
